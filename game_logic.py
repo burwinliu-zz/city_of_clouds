bf51581396e9bbbf4619f109b1d05e31bcb6d27e
18xulisa <36091363+18xulisa@users.noreply.github.com>
2018-12-01 12:28:37 -0800

    lightning and octocat

--- game_logic.py
+++ game_logic.py
@@ -20,7 +20,6 @@ class Game:
         self._landed = False
         self._state = list()
         self._lighting = list()
-        self._cat_pos = [0, 0]
         self._building_height = building
         self._column = column
         self._row = row
@@ -107,17 +106,6 @@ class Game:
         """
         return self._format_print_game()[0]
 
-    def set_lightning(self, falling_pos: int):
-        pass
-
-    def search(self, parameter: int):
-
-    def move_left(self):
-        self._cat_pos[1] -= 1
-
-    def move_right(self):
-        self._cat_pos[1] += 1
-
     def update_game(self) -> None:
         """
         :return: None
@@ -125,10 +113,34 @@ class Game:
         Updates the game according to necessary requirements and fulfills all necessary steps to
         move the game forward according to everything that has been changed
         """
-        if self._state[self._cat_pos[0]][self._cat_pos[1]] != CAT:
-
-            pass
+        self._check_fallers_and_landed()
+        if self._game_over:
+            print("GAME OVER")
+            return
+        to_change = self._detect_change()
+        if self._has_bottom():
+            if self._landed:
+                for temp in to_change:
+                    self._state[temp[0]][temp[1]] = FALLING
+                self.print_game()
+                if self._game_over:
+                    print("GAME OVER")
+                self._landed = False
+                return
+        for temp in to_change:
+            self._game[temp[0] + 1][temp[1]] = temp[2]
+            self._state[temp[0] + 1][temp[1]] = FALLING
+        if self._test_bottom(0, self._column_drop):
+            self.print_game()
+            self._game_over = True
+            print("GAME OVER")
+            return
+        self._state[0][self._column_drop] = FALLING
+        self._board_fall()
+        if not self._landed:
+            self._check_bottom_and_set_to_landed()
         self.print_game()
+        self._check_fallers_and_landed()
 
     def print_game(self) -> None:
         """
@@ -145,6 +157,32 @@ class Game:
         under_print += '-' * counter_row
         under_print += ' '
         print(under_print)
+
+    def _board_fall(self) -> None:
+        """
+        :return: None
+
+        sets the boards item to falling
+        """
+        self._has_faller = True
+        self._landed = False
+        for row in range(self._row):
+            for column in range(self._column):
+                if self._state[row][column] != EMPTY:
+                    self._state[row][column] = FALLING
+
+    def _board_landed(self) -> None:
+        """
+        :return: None
+
+        sets the board's item to landed
+        """
+        self._has_faller = False
+        self._landed = True
+        for row in range(self._row):
+            for column in range(self._column):
+                if self._state[row][column] != EMPTY:
+                    self._state[row][column] = ON_FIRE
 
     def _test_bottom(self, target_row: int, column: int) -> bool:
         """
@@ -167,13 +205,31 @@ class Game:
 
         check if it in the faller has a bottom underneath it
         """
-        if self._landed:
+        self._check_fallers_and_landed()
+        if self._has_faller or self._landed:
             item = self._find_item(None)
         else:
             return False
         if len(item) != 0:
             interesting_item = item[-1]
             return self._test_bottom(interesting_item[0] + 1, interesting_item[1])
+
+    def _detect_change(self) -> [[int, int, str]]:
+        """
+        :return: [[int, int, str]]
+
+        returns what is falling and their values in the self._game
+        """
+        row = 0
+        to_change = []
+        while row < self._row:
+            column = 0
+            while column < self._column:
+                if self._state[row][column] == FALLING:
+                    to_change.append([row, column, self._state[row][column]])
+                column += 1
+            row += 1
+        return to_change
 
     def _format_print_game(self) -> [[[str]], int]:
         """
@@ -241,4 +297,45 @@ class Game:
                     result.append([row, column, self._state[row][column]])
         return result
 
+    def _check_fallers_and_landed(self) -> None:
+        """
+        :return: None
+
+        check if the items are falling or landed or matched or frozen and adjusts
+        the states accordingly
+        """
+        for rows in self._state:
+            for elements in rows:
+                if elements == FALLING:
+                    self._has_faller = True
+                    self._landed = False
+                    self._board_fall()
+                    return
+                elif elements == ON_FIRE:
+                    self._landed = True
+                    self._has_faller = False
+                    self._board_landed()
+                    return
+        self._has_faller = False
+        self._landed = False
+        self._match = False
+
+    def _check_bottom_and_set_to_landed(self):
+        """
+        :return: None
+
+        makes sure that as they land they are set to frozen
+        """
+        item = self._find_item(FALLING)
+        change_items = False
+        for element in item:
+            if element[0] + 1 == self._row:
+                change_items = True
+            elif self._state[element[0] + 1][element[1]] == EMPTY:
+                change_items = True
+        if change_items:
+            self._landed = True
+            self._has_faller = False
+            for element in item:
+                self._state[element[0]][element[1]] = ON_FIRE
 
