b27eec99ae1d4402aa4f5ff4bbd1cad6734660b0
18xulisa <36091363+18xulisa@users.noreply.github.com>
2018-12-01 11:59:59 -0800

    lightning and octocat

---
+++ lightning.py
@@ -0,0 +1,13 @@
+import pygame
+
+class Lightning():
+    def __init__(self, screen):
+        self.screen = screen
+
+        #load the lightning  image
+        self.image = pygame.image.load('')
+
+        #get the lightning rectangle
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
---
+++ octocat.py
@@ -0,0 +1,33 @@
+import pygame
+
+class Ship():
+    def __init__(self, screen):
+        self.image = pygame.image.load('')
+        self.screen = screen
+
+        #get ship rectangle
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect
+
+        #start octocat on clouds
+        self.rect.centerx = 4
+
+        #store a decimal value for the ship's center
+        self.center = float(self.rect.centerx)
+
+        #movement flags
+        self.move_right = False
+        self.move_left = False
+
+    def update(self):
+        if self.move_right and self.rect.right < self.screen_rect.right:
+            self.center += 1
+        if self.move_left and self.rect.left > 0:
+            self.center -= 1
+
+        #update rect object
+        self.rect.centerx = self.center
+
+    def blitme(self):
+        """draws octocat at current location"""
+        self.screen.blit(self.image, self.rect)
